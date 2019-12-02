91b5c1d56fb100ee1706fb1e3fd2d2daefd6fb70
Oleksii Kosobutskyi <oleksii.kosobutskyi.stud@pw.edu.pl>
2019-12-02 21:52:48 +0100

    Lab 7

--- lab_7/task_3.py
+++ lab_7/task_3.py
@@ -6,8 +6,8 @@ def estimate_pi(n):
     Returns estimated value of pi.
 
     Funkcja szacuje wartość pi metodą probabilistyczną.
-    Wygenerujmy m punktów z obszaru [-1,1]^2. Niech k określa liczbę punktów
-    odległych od punku (0,0) o nie więcej niż 1. Proporcja 4k/m
+    Wygenerujmy n punktów z obszaru [-1,1]^2. Niech k określa liczbę punktów
+    odległych od punku (0,0) o nie więcej niż 1. Proporcja 4k/n
     powinna szacować wartość pi.
     (1pkt).
 
@@ -18,7 +18,7 @@ def estimate_pi(n):
     """
     x = np.random.uniform(-1, 1, size=(2, n))
     k = np.sum(np.sqrt(x[0]**2 + x[1]**2) < 1)
-    return 4* k / n
+    return 4 * k / n
 
 
 if __name__ == '__main__':
